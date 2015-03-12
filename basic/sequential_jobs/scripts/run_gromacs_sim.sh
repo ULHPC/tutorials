1a1f66b83e45cd0be48d01c52360693ed5db7445
Hyacinthe Cartiaux <Hyacinthe.Cartiaux@uni.lu>
2015-03-12 15:01:21 +0100

    sequential_jobs/scripts: gromacs example script

---
+++ basic/sequential_jobs/scripts/run_gromacs_sim.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+
+VALUE=$1
+SRC=ftp://ftp.gromacs.org/pub/benchmarks/rnase_bench_systems.tar.gz
+
+cd /tmp
+
+# extract the example input files
+
+if [[ ! -d rnase_cubic ]] ; then
+  wget $SRC
+  tar xzvf rnase_bench_systems.tar.gz rnase_cubic
+fi
+
+# Create a new workdir
+
+cp -R rnase_cubic /tmp/rnase_cubic_fs_$VALUE
+cd /tmp/rnase_cubic_fs_$VALUE
+
+# Load Gromacs
+
+module load GROMACS/4.6.1-goolf-1.4.10-mt
+
+# Generate a parameter file based on the value of the first parameter of this script
+
+sed -i "s/^fourier_spacing.*$/fourier_spacing=${VALUE}/" pme_verlet.mdp
+grompp -f pme_verlet.mdp -c conf.gro -p topol.top -o bench_rnase_cubic.tpr
+mdrun -s bench_rnase_cubic.tpr -nsteps 1000
+
