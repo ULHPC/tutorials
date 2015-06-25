077a1117dfd6d63c6c821a425386c3b74153dd47
Hyacinthe Cartiaux <Hyacinthe.Cartiaux@uni.lu>
2015-06-26 00:06:34 +0200

    Advanced parametric jobs: array job script

---
+++ advanced/advanced_parametric_jobs/scripts/array_job.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+echo "hostname : " `hostname`
+echo "OAR_JOB_NAME    : ${OAR_JOB_NAME}"
+echo "OAR_JOB_ID      : ${OAR_JOB_ID}"
+echo "OAR_ARRAY_ID    : ${OAR_ARRAY_ID}"
+echo "OAR_ARRAY_INDEX : ${OAR_ARRAY_INDEX}"
