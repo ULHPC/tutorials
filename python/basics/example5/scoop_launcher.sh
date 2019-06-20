f7b228ec27b9dba01a242bed027965f9094db04a
Clément Parisot <clement.parisot@uni.lu>
2019-06-20 16:24:28 +0200

    [python/basics] scoop - Update submission files
    Signed-off-by: Clément Parisot <clement.parisot@uni.lu>

--- python/basics/example5/scoop_launcher.sh
+++ python/basics/example5/scoop_launcher.sh
@@ -3,19 +3,25 @@
 #SBATCH --output=scoop_%j.log
 #
 #SBATCH -N 2
-#SBATCH --cpus-per-task=1
+#SBATCH -n 56
+#SBATCH -c 1
 #SBATCH --time=0-00:35:00
 #
 #SBATCH -p batch
 #SBATCH --qos=qos-batch
 #
 #SBATCH --exclusive
+#SBATCH -d singleton
 #SBATCH -m cyclic
 #SBATCH -C skylake
+#
+#SBATCH --array=1-55
 #
 # Ensure process affinity is disabled
 export SLURM_CPU_BIND=none
 hosts=$(srun bash -c hostname)
 echo $hosts
-echo $((SLURM_NTASKS-1))
-python -m scoop --hosts $hosts -n $((SLURM_NTASKS-1)) pi_calc.py $((SLURM_NTASKS-1))
+NB_WORKERS=$SLURM_ARRAY_TASK_ID
+echo $NB_WORKERS
+python -m scoop --hosts $hosts -n $NB_WORKERS pi_calc.py $NB_WORKERS
+
