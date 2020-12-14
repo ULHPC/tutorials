31dbe067cb3d2af674ae43a8a6497bd7ae5c113d
ezhilmathi krishnasamy <ezhilmathi.krishsnasamy@uni.lu>
2020-12-14 19:14:42 +0100

    [PS05] binary files are deleted
    Signed-off-by: ezhilmathi krishnasamy <ezhilmathi.krishsnasamy@uni.lu>

--- parallel/mpi/examples/scripts/batch.sh
+++ parallel/mpi/examples/scripts/batch.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks-per-node=7
 #SBATCH --time=00:15:00
 #SBATCH -p batch
-#SBATCH -A ulhpc
+#SBATCH --reservation=hpcschool
 
 module purge
 module load swenv/default-env/latest
--- parallel/openmp/profiling/ARM/env-module.sh
+++ parallel/openmp/profiling/ARM/env-module.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 module purge
 module load swenv/default-env/latest
-module load tools/ArmForge/19.1
 #module load toolchain/foss/2019a
 module load toolchain/intel/2019a
+module load tools/ArmForge/19.1
